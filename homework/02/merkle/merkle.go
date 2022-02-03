6058da274a37fd088654f788981fb6603f2c3b06
Timothy Dodd <77984950+timothydodd3@users.noreply.github.com>
2022-02-03 01:06:08 -0700

    mine done, merkle next

--- homework/02/merkle/merkle.go
+++ homework/02/merkle/merkle.go
@@ -1,38 +1,51 @@
 package merkle
 
-// TODO - uncomment the imports when you build your code.
-/*
 import (
 	"fmt"
 
 	"github.com/Univ-Wyo-Education/S22-4010/homework/02/hash"
 )
-*/
 
 func MerkleHash(data [][]byte) []byte {
-	// Replace this line with your code
-	/*
-	   ### Pseudo Code
+	var hTmp [][]byte = data[0:(len(data))]
 
-	   1. Create a slice to hold the hashes of the leaves.  Each leaf hash is a `[]byte`.  So make the data type `[][]byte`.
-	   	Make this slice of slice of byte then length of the data.  That would be `len(data)`.  Let's call this `hTmp`.
-	   2. For each data block
-	   	1. Calculate a hash for the data block using `hash.HashOf()`.
-	   	2. Save this in the slice created in (1) above.
-	   3. Create a `[][]byte` slice to hold the intermediate hashes in the tree.
-	   	This will need to be no more than `len(data)/2+1` in length.  The plus 1 is so that 0 blocks of hasing or an odd
-	   	number of blocks will have enough space.  Let's call this `hMid`.
-	   4. Declare a variable `ln`, and set it to  `len(data)/2+1`
-	   5. While `ln >= 1` (Hint: the language only has `for` loops with lots of different ways of doing it)
-	   	1. For each pair of hashes (if you have an odd number just use the single hash)
-	   		- Calculate the hash of the pair using `hash.Keccak256()`.  It takes a variable number of arguments so you can
-	   		  pass 1 or 2 arguments to it.
-	   		- Append this to `hMid`.
-	   	2. Replace hTmp with hMid
-	   	3. Recalculate `ln` set it to `len(hTmp)/2`
-	   	4. Generate a new empty hMid of allocated space of `len(hTmp)/2`.
-	   6. Return `hTmp[0]`
-	*/
-	// return []byte{0x1} // remove this line - repalce with your own return
-	return InstructorMerkleHash(data)
+	for i := 0; i < len(data); i++ {
+		theHashOfData := hash.HashOf(data[i])
+		hTmp[i] = theHashOfData
+	}
+	ln := (len(data) / 2) + 1
+	var hMid [][]byte = data[0:ln]
+
+	for j := 0; ln >= 3; j++ {
+		fmt.Printf("hMid is [%d]", ln)
+		tempHash := hash.Keccak256(hTmp[j], hMid[j])
+		hMid = append(hMid, tempHash)
+		hTmp = hMid
+		ln = len(hTmp) / 2
+	}
+	return hTmp[0]
 }
+
+/*
+   ### Pseudo Code
+
+   1. Create a slice to hold the hashes of the leaves.  Each leaf hash is a `[]byte`.  So make the data type `[][]byte`.
+   	Make this slice of slice of byte the length of the data.  That would be `len(data)`.  Let's call this `hTmp`.
+   2. For each data block
+   	1. Calculate a hash for the data block using `hash.HashOf()`.
+   	2. Save this in the slice created in (1) above.
+   3. Create a `[][]byte` slice to hold the intermediate hashes in the tree.
+   	This will need to be no more than `len(data)/2+1` in length.  The plus 1 is so that 0 blocks of hasing or an odd
+   	number of blocks will have enough space.  Let's call this `hMid`.
+   4. Declare a variable `ln`, and set it to  `len(data)/2+1`
+   5. While `ln >= 1` (Hint: the language only has `for` loops with lots of different ways of doing it)
+   	1. For each pair of hashes (if you have an odd number just use the single hash)
+   		- Calculate the hash of the pair using `hash.Keccak256()`.  It takes a variable number of arguments so you can
+   		  pass 1 or 2 arguments to it.
+   		- Append this to `hMid`.
+   	2. Replace hTmp with hMid
+   	3. Recalculate `ln` set it to `len(hTmp)/2`
+   	4. Generate a new empty hMid of allocated space of `len(hTmp)/2`.
+   6. Return `hTmp[0]`
+*/
+// return []byte{0x1} // remove this line - repalce with your own return
--- homework/02/mine/mine.go
+++ homework/02/mine/mine.go
@@ -1,60 +1,56 @@
 package mine
 
-import block "github.com/Univ-Wyo-Education/S22-4010/homework/02/block"
+//Timothy Dodd
+//02/02/2022
+//COSC 4010: Assignemnt 2. Mining and Merkle Hash, Part1: Mining
 
 // TODO Replace above import with import below (commented out)
-/*
 import (
 	"encoding/hex"
 	"fmt"
 
+	// "hash"
+
 	"github.com/Univ-Wyo-Education/S22-4010/homework/02/block"
 	"github.com/Univ-Wyo-Education/S22-4010/homework/02/hash"
 )
-*/
 
-// MineBlock implements a proof of work mining system where the first 4
-// digits (2 bytes) of the hex value are 0.  This is set with the difficulty
-// parameter.  So it could be more than 4 or less (Test with less).
-// Difficulty can be increaesed by requiring more digits to be 0 or by
-// requring some other pattern to apear in the resulting hash.
 func MineBlock(bk *block.BlockType, difficulty string) {
-	// Pseudo-Code:
-	//
-	// 1. Use an infinite loop to:
-	//   1. Serialize the data from the block for hashing, Call
-	//     `block.SearilizeForSeal` to do this.
-	//   2. Calculate the hash of the data, Call `hash.HashOf` to do this.
-	//      This is the slow part.  What would happen if we replaced the
-	//      software with a hash calculator on a graphics card where you
-	//      could run 4096 - 10240 hahes at once?  What would happen if we
-	//      replaced the graphics card with an ASIC - so you had dedicated
-	//		hardware to do the hash and you could run 4 billion hashes a
-	//      second?
-	//   3. Convert the hash (it is []byte) to a hex string.  Use the
-	//      `hex.EncodeToString` standard go library function.
-	//   4. `fmt.Printf("((Mining)) Hash for Block [%s] nonce [%8d]\r",
-	//      theHashAsAString, bk.Nonce)` `\r` will overwrite the same line
-	//      instead of advancing to the next. (You may want to skip on
-	//      some windows systems)
-	//   5. See if the first 4 characters of the hash are 0's. - if so we
-	//      have met the work criteria.  In go this is
-	//     `if theHashAsAString[0:n] == difficulty ("0000" for example) {`.
-	//      This is create a slice, 4 long from character 0 with length of 4,
-	//      then compare that to the string `difficulty`.
-	//    -   Set the block's "Seal" to the hash
-	//    -   `fmt.Printf("((Mining)) Hash for Block [%s] nonce [%8d]\n",
-	//        theHashAsAString, bk.Nonce)` `\n` will overwrite the same
-	//        *and then advance* to the next line.
-	//    -   return
-	//   5. Increment the Nonce in the block, and...
-	//   6. Back to the top of the loop for another try at finding a seal
-	//      for this block.
-	//
-	// For the genesis block, when I do this it requires 54586 trips through
-	// the loop to calculate the proof of work (PoW).
-	//
+	for {
+		//Gets the Hasf Value of the Block
+		theHashOfTheBlock := block.SerializeForSeal(bk)
+		//Hashes the Value Serialized
+		theHashAsAHash := hash.HashOf(theHashOfTheBlock)
+		//Converts Hash Value to a String for printing out
+		theHashAsAString := hex.EncodeToString(theHashAsAHash)
+		//Prints the return value of the Hash and the current attempt
+		fmt.Printf("Hash for block [%s] nonce [%8d]\r", theHashAsAString, bk.Nonce)
+		//Difficulty determines the first 4 digits of correct hash
+		difficulty = "0000"
+		n := 4
+		if theHashAsAString[0:n] == difficulty {
+			//Sets the seal as the correct hash
+			bk.Seal = theHashAsAHash
+			//Prints the value of the correct hash and nonce
+			fmt.Printf("((Mining)) Hash Block for [%s] nonce [%8d]\n", theHashAsAString, bk.Nonce)
+			return
+		}
+		//Will increment the loop once all checks are done
+		bk.Nonce++
+	}
 
-	// TODO: Start coding here.
-	// InstructorImplementationMineBlock(bk, difficulty)
 }
+
+//Output given from the running go test on this file...
+// timothydodd@Timothys-MacBook-Pro 02 % cd ./main
+// timothydodd@Timothys-MacBook-Pro main % go build
+// timothydodd@Timothys-MacBook-Pro main % cd '/Users/timothydodd/Documents/GitHub/S22-4010/homework/02/mine'
+// timothydodd@Timothys-MacBook-Pro mine % go test
+// ((Mining)) Hash Block for [0000ae2cab130b4836988969f731c4f884ac4675790e5575a5161e5b96ab13d7] nonce [   54586]
+// ((Mining)) Hash Block for [0000adc29a80f1f0df08c8687c013d179050f5d1b449599e4d1437e4fad23525] nonce [   46734]
+// ((Mining)) Hash Block for [000013ce557332aaa68abe3b7bf1be856743a03689a802606a732e81713bb78c] nonce [    4527]
+// PASS
+// ok      github.com/Univ-Wyo-Education/S22-4010/homework/02/mine 1.971s
+
+//As for the qestion raised by Point 2 in the Pseudocode, Replacing the code with a GPU would make the code run far faster.
+//And Replacing that with ASIC hardware dedicated to doing the hash would likely make it instantainous
