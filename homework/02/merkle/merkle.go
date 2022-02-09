dc9345d70797b795bb9a849f94c6472b5e14cb48
Timothy Dodd <77984950+timothydodd3@users.noreply.github.com>
2022-02-09 12:41:18 -0700

    merkle.go Works

--- homework/02/merkle/merkle.go
+++ homework/02/merkle/merkle.go
@@ -1,52 +1,47 @@
 package merkle
 
+//Timothy Dodd: COSC 4010
+//Assignment2: Merkle.go
+//This does a merkle hash
+
 import (
 	"github.com/Univ-Wyo-Education/S22-4010/homework/02/hash"
 )
 
 func MerkleHash(data [][]byte) []byte {
-	var hTmp [][]byte = data[0:len(data)]
+	var hTmp [][]byte = make([][]byte, len(data))
 
 	for i := 0; i < len(data); i++ {
-		theHashOfData := hash.HashOf(data[i])
-		hTmp[i] = theHashOfData
+		hTmp[i] = hash.HashOf(data[i])
 		// theHashAsAString := hex.EncodeToString(hTmp[i])
 		// fmt.Printf("The has as String [%s] at point [%d]\r", theHashAsAString, i)
 	}
 	ln := len(data)/2 + 1
-	var hMid [][]byte = data[0:ln]
+	var hMid [][]byte = make([][]byte, 0, ln)
 	// fmt.Printf("Length of Data [%d]\n", ln)
-
-	for i := 0; i >= ln && ln >= 1; i++ {
-		tempHash := hash.Keccak256(hTmp[i], hMid[i])
-		hMid = append(hMid, tempHash)
+	if ln == 1 {
+		tempHash := hash.Keccak256(hTmp[0])
+		return tempHash
+	}
+	for ln >= 1 {
+		for i := 0; i < len(hTmp); i = i + 2 {
+			//fmt.Printf("Length of hMid [%d]\n", len(hTmp))
+			if (i + 1) >= len(hTmp) {
+				tempHash := hash.Keccak256(hTmp[i])
+				hMid = append(hMid, tempHash)
+				// theHashAsAString := hex.EncodeToString(hMid[i])
+				// fmt.Printf("Choice 1 as String [%s] at point [%d]\n", theHashAsAString, i)
+			} else {
+				tempHash := hash.Keccak256(hTmp[i], hTmp[i+1])
+				hMid = append(hMid, tempHash)
+				// theHashAsAString := hex.EncodeToString(hMid[i])
+				// fmt.Printf("Choice 2 as String [%s] at point [%d]\n", theHashAsAString, i)
+			}
+		}
 		hTmp = hMid
 		ln = len(hTmp) / 2
-		hMid = hMid[0:ln]
+		hMid = make([][]byte, 0, ln)
+		// fmt.Printf("The Value of ln is [%d]\n", ln)
 	}
 	return hTmp[0]
 }
-
-/*
-   ### Pseudo Code
-
-   1. Create a slice to hold the hashes of the leaves.  Each leaf hash is a `[]byte`.  So make the data type `[][]byte`.
-   	Make this slice of slice of byte the length of the data.  That would be `len(data)`.  Let's call this `hTmp`.
-   2. For each data block
-   	1. Calculate a hash for the data block using `hash.HashOf()`.
-   	2. Save this in the slice created in (1) above.
-   3. Create a `[][]byte` slice to hold the intermediate hashes in the tree.
-   	This will need to be no more than `len(data)/2+1` in length.  The plus 1 is so that 0 blocks of hasing or an odd
-   	number of blocks will have enough space.  Let's call this `hMid`.
-   4. Declare a variable `ln`, and set it to  `len(data)/2+1`
-   5. While `ln >= 1` (Hint: the language only has `for` loops with lots of different ways of doing it)
-   	1. For each pair of hashes (if you have an odd number just use the single hash)
-   		- Calculate the hash of the pair using `hash.Keccak256()`.  It takes a variable number of arguments so you can
-   		  pass 1 or 2 arguments to it.
-   		- Append this to `hMid`.
-   	2. Replace hTmp with hMid
-   	3. Recalculate `ln` set it to `len(hTmp)/2`
-   	4. Generate a new empty hMid of allocated space of `len(hTmp)/2`.
-   6. Return `hTmp[0]`
-*/
-// return []byte{0x1} // remove this line - repalce with your own return
